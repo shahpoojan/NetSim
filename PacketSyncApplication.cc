d97c896455f3da97891d8c9aecb55c34c61ffbfc
cpatel60 <cpatel60@gatech.edu>
2012-11-27 15:43:49 -0500

    Update PacketSyncApplication.cc

---
+++ PacketSyncApplication.cc
@@ -0,0 +1,12 @@
+#include"PacketSyncApplication.h"
+
+PacketSyncApplication::PacketSyncApplication():Application()
+{
+      totalPacketsReceived=0; // statistics
+      totalBytesReceived=0;
+}
+
+PacketSyncApplication::SetPeerAddress(int peerAddr)
+{
+  
+}
